98f26b5e68927e0b3039c33fbcb978d239b90867
daram62 <97465078+daram62@users.noreply.github.com>
2024-09-15 19:55:58 +0900

    solve 2nd hw_DS

---
+++ Data Structures/quiz4.c
@@ -0,0 +1,44 @@
+// Let length[i] be the desired length of row i of a two-dimensional array. 
+// Write a function similar to make2dArray() to create a two-dimensional array 
+// such that row i has length[i] elements.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int** makeJaggedArray(int* len, int row) {
+    int** array = (int**) malloc (row * sizeof(int*));
+
+    for (int i = 0; i < row; i++) {
+        array[i] = (int*)malloc(len[i] * sizeof(int));
+    }
+    return array;
+}
+
+void freeArray(int** array, int row) {
+    for (int i = 0; i < row; i++) {
+        free(array[i]);
+    }
+    free(array);
+}
+
+int main() {
+    int row = 5;
+    int len[] = {1,2,3,4,5};
+
+    int** J_Array = makeJaggedArray(len, row);
+
+    J_Array[0][0] = 10;
+    J_Array[1][1] = 20;
+    J_Array[2][2] = 30;
+    J_Array[3][3] = 40;
+    J_Array[4][4] = 50;
+
+    printf("%d\n", J_Array[0][0]);
+    printf("%d\n", J_Array[1][1]);
+    printf("%d\n", J_Array[2][2]);
+    printf("%d\n", J_Array[3][3]);
+
+    freeArray(J_Array, row);
+
+    return 0;
+}
---
+++ Data Structures/quiz5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Rectangle {
+    int length;
+    int width;
+};
+
+struct Triangle {
+    int base;
+    int height;
+};
+
+struct Circle {
+    float radius;
+};
+
+int main() {
+    Rectangle rect = {10, 5};
+    Triangle tri = {5, 4};
+    Circle cir = {7.0};
+
+}
